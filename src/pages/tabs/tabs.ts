eb5c39fb87ee55e94601121a7af289d6944153b9
fred-penot <frederic.penot@gmail.com>
2017-03-12 15:13:21 +0100

    20170312-002

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,24 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Tabs } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { RemotePage } from '../remote/remote';
 import { VocalPage } from '../vocal/vocal';
 import { SettingPage } from '../setting/setting';
 import { CommonService } from '../../providers/common-service';
+import { GallyCommandService } from '../../providers/gally-command-service';
 import { RemoteService } from '../../providers/remote-service';
 
 @Component({
   templateUrl: 'tabs.html',
-  providers: [Storage, CommonService, RemoteService]
+  providers: [Storage, CommonService, RemoteService, GallyCommandService]
 })
 export class TabsPage {
+  @ViewChild('myTabs') tabRef: Tabs;
+
   // this tells the tabs component which Pages
   // should be each tab's root Page
   tab1Root: any = RemotePage;
   tab2Root: any = VocalPage;
   tab3Root: any = SettingPage;
 
-  constructor(public remoteService: RemoteService) {
+  constructor(public remoteService: RemoteService, public commonService: CommonService,
+              public gallyCommandService: GallyCommandService) {
 
+  }
+
+  ionViewDidEnter() {
+    this.gallyCommandService.getParam().then(dataParam => {
+      if (dataParam['statut']) {
+        let param = dataParam['data']['data']['param'];
+        if (param['sexe'] == undefined && param['name'] == undefined
+            && param['birth_timestamp'] == undefined && param['birth_cp'] == undefined
+            && param['birth_city'] == undefined && param['current_cp'] == undefined
+            && param['current_city'] == undefined) {
+          this.commonService.toastShow('Veuillez remplir les paramètres avant toute utilisation.');
+          this.tabRef.select(2);
+        }
+      } else {
+        this.commonService.toastShow('Veuillez remplir les paramètres avant toute utilisation.');
+        this.tabRef.select(2);
+      }
+    });
   }
 
   selectTab(tab) {
--- src/pages/vocal/vocal.ts
+++ src/pages/vocal/vocal.ts
@@ -1,19 +1,93 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { CommonService } from '../../providers/common-service';
+import { SpeechExecService } from '../../providers/speech-exec-service';
+import { GallyCommandService } from '../../providers/gally-command-service';
+import { SpeechCommandService } from '../../providers/speech-command-service';
+import { SpeechService } from '../../providers/speech-service';
 
 @Component({
   selector: 'page-vocal',
-  templateUrl: 'vocal.html'
+  templateUrl: 'vocal.html',
+  providers: [CommonService, SpeechExecService, GallyCommandService, SpeechCommandService, SpeechService]
 })
 export class VocalPage {
+  private mic: string;
+  private micSelect: any = {'on': 'mic', 'off': 'mic-off'};
+  private gally: any;
 
-  constructor(public navCtrl: NavController) {
 
+  constructor(public navCtrl: NavController, public platform: Platform, public commonService: CommonService,
+              private gallyCommandService: GallyCommandService, public speechService: SpeechService) {
+    this.mic = this.micSelect.off;
+    this.gally = this.speechService;
   }
 
-  test() {
-    console.log('ok');
+  ionViewDidEnter() {
+    this.commonService.loadingShow('Please wait...');
+    this.checkParam().then(dataParam => {
+      if (dataParam) {
+        this.gallyCommandService.getVocalCommands().then(dataCommand => {
+          if ( dataCommand['statut'] ) {
+            var countCommands = dataCommand['data']['data']['command'].length;
+            for (var i = 0; i < countCommands; i++) {
+              this.gally.addCommand(dataCommand['data']['data']['command'][i]);
+            }
+            this.gally.getCountCommand();
+          } else {
+            this.commonService.toastShow(dataCommand['message']);
+          }
+          this.commonService.loadingHide();
+        });
+      } else {
+        this.commonService.toastShow('Veuillez remplir les paramètres avant toute utilisation.');
+        this.navCtrl.parent.select(2);
+        this.commonService.loadingHide();
+      }
+    });
   }
 
+  ionViewWillLeave() {
+    this.mic = this.micSelect.off;
+    this.gally.stop();
+    this.gally.clearCommand();
+  }
+
+  checkParam() {
+    return this.gallyCommandService.getParam().then(dataParam => {
+      if (dataParam['statut']) {
+        let param = dataParam['data']['data']['param'];
+        if (param['sexe'] != undefined && param['name'] != undefined
+            && param['birth_timestamp'] != undefined && param['birth_cp'] != undefined
+            && param['birth_city'] != undefined && param['current_cp'] != undefined
+            && param['current_city'] != undefined) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    });
+  }
+
+  activate() {
+    this.checkParam().then(dataParam => {
+      if (dataParam) {
+        this.platform.ready().then(() => {
+          if ( this.gally.isDefine() ) {
+            this.mic = this.micSelect.off;
+            this.gally.stop();
+          } else {
+            this.gally.setDefine(true);
+            this.mic = this.micSelect.on;
+            this.gally.record();
+          }
+        });
+      } else {
+        this.commonService.toastShow('Veuillez remplir les paramètres avant toute utilisation.');
+        this.navCtrl.parent.select(2);
+      }
+    });
+  }
 }
